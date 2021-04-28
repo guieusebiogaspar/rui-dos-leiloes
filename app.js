a360a204784019d9da08c5c57a80327dae40d5e8
guieusebiogaspar <guicr9@hotmail.com>
2021-04-28 20:01:09 +0100

    ficheiro env com password

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ const initPassport = require("./passportConfig.js")
 
 const leiloesRoutes = require("./api/routes/leiloes");
 const usersRoutes = require("./api/routes/users");
-require("dotenv").config();
-
+//require("dotenv").config();
 const app = express();
 
 /*
