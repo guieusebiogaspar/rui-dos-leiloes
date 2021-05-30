09735a004f31c67332ac92824f903d82b934e80d
guieusebiogaspar <guicr9@hotmail.com>
2021-05-30 03:00:15 +0100

    falta so o termino do leilao e as cenas do admin

--- api/routes/leiloes.js
+++ api/routes/leiloes.js
@@ -11,20 +11,24 @@ router.post("/leilao", verifyToken, LeiloesController.cria_leilao)
 router.get("/", verifyToken, LeiloesController.get_leiloes);
 
 // listar leilões existentes com o ean indicado
-router.get("/:ean", verifyToken, LeiloesController.get_ean);
+router.get("/:ean", verifyToken, LeiloesController.get_ean)
 
 // listar leilao com o id indicado
-// AQUI FALTA METER AS MENSAGENS E HISTORICO DAS LICITAÇÕES
 router.get("/leilao/:leilaoid", verifyToken, LeiloesController.get_leilao);
 
 // listar leilões em que user tenha atividade
+router.get("/user/:userid", verifyToken, LeiloesController.get_leiloes_user)
 
 // efetuar uma licitação num leilão
+router.post("/leilao/:leilaoid", verifyToken, LeiloesController.post_licitacao)
 
 // editar propriedades de um leilão
-// AQUI FALTA METER AS VERSOES NO HISTORICO
-router.put("/leilao/:leilaoid", verifyToken, LeiloesController.editar_leilao)
+router.put("/leilao/:leilaoid", verifyToken, LeiloesController.put_editar_leilao)
+
+// escrever mensagem no mural de um leilão
+router.post("/leilao/mural/:leilaoid", verifyToken, LeiloesController.post_mural)
 
 
+// admin cancela leilão
 
 module.exports = router;
